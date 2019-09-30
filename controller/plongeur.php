1d58e92e19b47a1720a0c933d9e69c625b66430c
thea <thealinot@gmail.com>
2019-09-30 16:46:01 +0200

    p'tites modifs

--- controller/plongeur.php
+++ controller/plongeur.php
@@ -12,7 +12,7 @@
     </thead>
     <tbody>
     <?php
-    $req = "select PER_NUM,PER_NOM,PER_PRENOM,APT_LIBELLE from PLO_PLONGEUR join PLO_PERSONNE using(PER_NUM) join PLO_APTITUDE using(APT_CODE)";
+    $req = "select PER_NUM,PER_NOM,PER_PRENOM,APT_LIBELLE from PLO_PLONGEUR join PLO_PERSONNE using(PER_NUM) join PLO_APTITUDE using(APT_CODE) order by PER_NUM";
     $db->LireDonneesPDO2($req, $tab);
    //AfficherDonnee2($tab);
     foreach($tab as $key=>$value){
