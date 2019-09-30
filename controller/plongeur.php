95071182e461cfb16df4e42a5d2616150f86fa3b
thea <thealinot@gmail.com>
2019-09-30 16:02:56 +0200

    les plongeurs s'affichent bien

--- controller/plongeur.php
+++ controller/plongeur.php
@@ -12,21 +12,27 @@
     </thead>
     <tbody>
     <?php
-    $req = "select * from PLO_PLONGEUR";
+    $req = "select PER_NUM,PER_NOM,PER_PRENOM,APT_LIBELLE from PLO_PLONGEUR join PLO_PERSONNE using(PER_NUM) join PLO_APTITUDE using(APT_CODE)";
     $db->LireDonneesPDO2($req, $tab);
-    AfficherDonnee2($tab);
+   //AfficherDonnee2($tab);
     foreach($tab as $key=>$value){
         echo "<tr>";
      foreach ($value as $val){
 
-    ?>
 
-        <td><?php echo $val; ?></td>
 
-    <?php }
-     echo "</tr>";
-    } ?>
+      echo  "<td>$val</td>";
+
+     }
+     echo '<td><a href="" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier la publications"><i class="material-icons white-text">remove_red_eye</i></a></td></tr>';
+     }  ?>
     </tbody>
 </table>
+
+<div class="fixed-action-btn">
+    <a class="btn-floating btn-large blue modal-trigger" href="/ProjetPlongee2A/public/?page=formNewPlongeur.php">
+        <i class="large material-icons">edit</i>
+    </a>
+</div>
 
 
