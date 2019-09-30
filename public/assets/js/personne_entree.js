b008d8cfcfd08a200fa070df32b3bb1cae601ed9
Quentin <21802953@etu.unicaen.fr>
2019-09-30 16:30:32 +0200

    Début des regex

--- public/assets/js/personne_entree.js
+++ public/assets/js/personne_entree.js
@@ -1,1 +1,6 @@
-alert("coucou thth");
+let prenom = document.getElementById("prenom").value;
+let nom = document.getElementById("nom").value;
+let regexPrenom = /[:alpha:]/i;
+let regexNom = /[:alpha:]/i;
+prenom.match(regexPrenom);
+prenom.match(regexNom);
