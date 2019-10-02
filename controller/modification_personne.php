34bf9fb06c6c4ca4fab4d44213c641161b784e61
Quentin <21802953@etu.unicaen.fr>
2019-10-02 11:16:37 +0200

    Modification des personnes

--- controller/modification_personne.php
+++ controller/modification_personne.php
@@ -3,10 +3,10 @@
 $sql = 'SELECT * FROM PLO_PERSONNE WHERE PER_NUM = '. $_GET['id'];
 $db->LireDonneesPDO2($sql, $resultat);
 
-include('../view/form/form_modification_personne.php');
+include_once('../view/form/form_modification_personne.php');
 ?>
 
     <script>
-        <?php include('../public/assets/js/personne_entree.js'); ?>
+        <?php include_once('../public/assets/js/personne_entree.js'); ?>
     </script>
 
--- view/form/form_modification_personne.php
+++ view/form/form_modification_personne.php
@@ -1,4 +1,4 @@
-<form action="../../controller/modification_personne.php">
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
     <label>
         Nom
         <input type="text" id="nom" name="nom" value="<?php echo $resultat[0]['PER_NOM'] ?>">
@@ -7,4 +7,14 @@
         Prénom
         <input type="text" id="prenom" name="prenom" value="<?php echo $resultat[0]['PER_PRENOM'] ?>">
     </label>
+    <button class="btn" name="submit">MODIFIER</button>
 </form>
+
+<?php
+    if ( isset($_POST['submit']) ) {
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $id = $_GET['id'];
+        $cur = $db->preparerRequetePDO("UPDATE PLO_PERSONNE SET PER_NOM = '$nom' , PER_PRENOM = '$prenom' WHERE PER_NUM = '$id'");
+        $db->majDonneesPrepareesPDO($cur);
+    }
