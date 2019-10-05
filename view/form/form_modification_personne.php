8d170366c8d9620aede93ec77b54956fe141dbc4
Quentin <21802953@etu.unicaen.fr>
2019-10-05 17:44:27 +0200

    Revert "Revert "Minor change""
    This reverts commit 1e945570

--- view/form/form_modification_personne.php
+++ view/form/form_modification_personne.php
@@ -24,36 +24,3 @@
 </form>
 
 <script type="text/javascript" src="assets/js/personne_entree.js"></script>
-<?php
-    include_once('../model/Traitement.php');
-
-    if ( isset($_POST['submit']) ) {
-        if ( !empty($_POST['nom']) && !empty($_POST['prenom']) ) {
-            $nom = strtoupper(enleverCaracteresSpeciaux($_POST['nom']));
-            $prenom = ucfirst(enleverCaracteresSpeciaux($_POST['prenom']));
-            $aptitude = $_POST['aptitude'];
-
-            $db->LireDonneesPDO2('SELECT * FROM PLO_PERSONNE', $personnes);
-
-            $nbPersonnes = count($personnes);
-
-            $i = 0;
-
-            // Si le prénom ou le nom a été modifié
-            if ($nom != $utilisateur[0]['PER_NOM'] || $prenom != $utilisateur[0]['PER_PRENOM'])
-                while (($nom != $personnes[$i]['PER_NOM'] || $prenom != $personnes[$i]['PER_PRENOM']) && ++$i < $nbPersonnes);
-            else
-                $i = $nbPersonnes;
-
-
-            if ($i == $nbPersonnes) {
-                $id = $_GET['id'];
-                $cur = $db->preparerRequetePDO("UPDATE PLO_PERSONNE SET PER_NOM = '$nom' , PER_PRENOM = '$prenom' WHERE PER_NUM = '$id'");
-                $db->majDonneesPrepareesPDO($cur);
-                $cur = $db->preparerRequetePDO("UPDATE PLO_PLONGEUR SET APT_CODE = '$aptitude' WHERE PER_NUM = '$id'");
-                $db->majDonneesPrepareesPDO($cur);
-                header("Location: ../public/?page=plongeur");
-            } else
-                echo "Personne déjà enregistrée.";
-        }
-    }
