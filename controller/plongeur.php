333a7f9aa78315d833c98f0100a730e569a7008b
Quentin <21802953@etu.unicaen.fr>
2019-10-05 16:45:42 +0200

    Minor change

--- controller/plongeur.php
+++ controller/plongeur.php
@@ -14,7 +14,7 @@
     <?php
     $req = "select PER_NUM,PER_NOM,PER_PRENOM,APT_LIBELLE from PLO_PLONGEUR join PLO_PERSONNE using(PER_NUM) join PLO_APTITUDE using(APT_CODE) order by PER_NUM";
     $db->LireDonneesPDO2($req, $tab);
-   //AfficherDonnee2($tab);
+
     foreach($tab as $key=>$value){
         echo "<tr>";
      foreach ($value as $val){
@@ -24,7 +24,7 @@
       echo  "<td>$val</td>";
 
      }
-     echo '<td><a href="/ProjetPlongee2A/public/?page=modification_personne&id='.$value["PER_NUM"].'" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier le plongeur"><i class="material-icons white-text">remove_red_eye</i></a></td></tr>';
+     echo '<td><a href="?page=modification_personne&id='.$value["PER_NUM"].'" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier le plongeur"><i class="material-icons white-text">remove_red_eye</i></a></td></tr>';
      }  ?>
     </tbody>
 </table>
