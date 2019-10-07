05a39769ba9500a9c78e7f8f9bc45feed403fb35
thea <thealinot@gmail.com>
2019-10-07 14:56:27 +0200

    suppression 'fini'

--- controller/plongeur.php
+++ controller/plongeur.php
@@ -1,7 +1,7 @@
 <?php
 
 ?>
-<div><h2 class="center">Liste des plongeurs</h2></div>
+<div>Liste des plongeurs</div>
 <table>
     <thead>
         <th>ID</th>
@@ -14,7 +14,7 @@
     <?php
     $req = "select PER_NUM,PER_NOM,PER_PRENOM,APT_LIBELLE from PLO_PLONGEUR join PLO_PERSONNE using(PER_NUM) join PLO_APTITUDE using(APT_CODE) order by PER_NUM";
     $db->LireDonneesPDO2($req, $tab);
-   //AfficherDonnee2($tab);
+
     foreach($tab as $key=>$value){
         echo "<tr>";
      foreach ($value as $val){
@@ -24,17 +24,15 @@
       echo  "<td>$val</td>";
 
      }
-<<<<<<<<< Temporary merge branch 1
-     echo '<td><a href="/ProjetPlongee2A/public/?page=modification_personne&id='.$value["PER_NUM"].'" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier la publications"><i class="material-icons white-text">remove_red_eye</i></a></td></tr>';
-=========
-     echo '<td><a href="/ProjetPlongee2A/public/?page=modification_personne&id='.$value["PER_NUM"].'" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier le plongeur"><i class="material-icons white-text">remove_red_eye</i></a></td></tr>';
->>>>>>>>> Temporary merge branch 2
-     }  ?>
+        echo '<td><a href="?page=modification_personne&id='.$value["PER_NUM"].'" class="waves-effect waves-light btn yellow accent-4 tooltipped" data-position="top" data-tooltip="Modifier le plongeur"><i class="material-icons white-text">remove_red_eye</i></a>';
+        include('../view/form/suppression_personne.php');
+        echo '</td></tr>';
+    }  ?>
     </tbody>
 </table>
 
 <div class="fixed-action-btn">
-    <a class="btn-floating btn-large blue modal-trigger" href="/ProjetPlongee2A/public/?page=formNewPlongeur">
+    <a class="btn-floating btn-large blue modal-trigger" href="?page=formNewPlongeur">
         <i class="large material-icons">edit</i>
     </a>
 </div>
--- view/form/suppression_personne.php
+++ view/form/suppression_personne.php
@@ -1,7 +1,20 @@
-<form method="post" action="<?php //$_SERVER[PHP_SELF] ?>" style="display: inline-block;" class="no-padding">
-    <input type="hidden" value="<?php $value["PER_NUM"] ?>">
-    <button class="btn red darken-4 waves-effect waves-light tooltipped" data-position="top" data-tooltip="Supprimer"><i class="material-icons white-text">delete</i></button>
+<form method="post" action="<?php $_SERVER["PHP_SELF"] ?>" style="display: inline-block;" class="no-padding">
+    <input type="hidden" value="<?php echo $value["PER_NUM"] ?>">
+    <button type="submit" name="<?php echo $value["PER_NUM"] ?>" class="btn red darken-4 waves-effect waves-light tooltipped" data-position="top" data-tooltip="Supprimer"><i class="material-icons white-text">delete</i></button>
 </form>
 <?php
+    $idUti = $value["PER_NUM"];
+    if(isset($_POST[$idUti])) {
 
+        $req2 = 'SELECT PAL_NUM FROM PLO_PLONGEUR JOIN PLO_CONCERNER USING(PER_NUM) JOIN PLO_PALANQUEE USING(PAL_NUM) WHERE PER_NUM = '.$idUti;
+        $db->LireDonneesPDO2($req2, $tab2);
+
+        if (count($tab2) == 0) {
+            $req3 = 'DELETE FROM PLO_PLONGEUR WHERE PER_NUM = '.$idUti;
+            $cur = $db->preparerRequetePDO($req3);
+            $db->majDonneesPrepareesPDO($cur);
+        } else {
+            echo "vous ne pouvez pas supprimer ce plongeur car il a des plongées";
+        }
+    }
 ?>
