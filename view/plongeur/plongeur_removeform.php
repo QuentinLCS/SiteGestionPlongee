42f5023757210a75e5720e5bcd74e14718d88619
thea <thealinot@gmail.com>
2019-10-08 20:50:45 +0200

    Inactif quand supprime plongeur avec plongées

--- view/plongeur/plongeur_removeform.php
+++ view/plongeur/plongeur_removeform.php
@@ -9,12 +9,16 @@
         $req2 = 'SELECT PAL_NUM FROM PLO_PLONGEUR JOIN PLO_CONCERNER USING(PER_NUM) JOIN PLO_PALANQUEE USING(PAL_NUM) WHERE PER_NUM = '.$idUti;
         $db->LireDonneesPDO2($req2, $tab2);
 
+        //vérifie si le plongeur a des plongées
         if (count($tab2) == 0) {
             $req3 = 'DELETE FROM PLO_PLONGEUR WHERE PER_NUM = '.$idUti;
             $cur = $db->preparerRequetePDO($req3);
             $db->majDonneesPrepareesPDO($cur);
         } else {
             echo "vous ne pouvez pas supprimer ce plongeur car il a des plongées";
+            $req4 = 'UPDATE PLO_PERSONNE SET PER_ACTIVE = 0 WHERE PER_NUM = '.$idUti;
+            $cur = $db->preparerRequetePDO($req4);
+            $db->majDonneesPrepareesPDO($cur);
         }
     }
 ?>
