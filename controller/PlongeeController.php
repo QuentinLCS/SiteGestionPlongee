6a6418471043e428c89a1a3d7cf699512ab303a5
Quentin <21802953@etu.unicaen.fr>
2019-10-12 15:56:27 +0200

    Page plongee 80% fonctionnelle

--- controller/PlongeeController.php
+++ controller/PlongeeController.php
@@ -4,5 +4,65 @@ require_once('_ControllerClass.php');
 
 class PlongeeController extends _ControllerClass
 {
+    private $plongeeManagers;
 
+    public function __construct($url)
+    {
+        $this->plongeeManagers = new PlongeeManager();
+
+        $urlSize = parent::__construct($url);
+
+        if ($urlSize > 1)
+            if($url[1] == 'show')
+                $this->show();
+            else
+                throw new Exception('Page introuvable');
+    }
+
+    /**
+     * Fonction chargée au chargement de la page.
+     * @throws Exception
+     */
+    public function index()
+    {
+        $this->add();
+
+        (new View('plongee/plongee_index'))->generate([
+            'allPlongees' => $this->plongeeManagers->getAll()
+        ]);
+    }
+
+    public function show()
+    {
+        if (!isset($_GET['plo_date']) || !isset($_GET['plo_matin_apresmidi']))
+            header('location: plongee');
+
+        $plongee = $this->plongeeManagers->getOne([
+            'PLO_DATE' => $_GET['plo_date'],
+            'PLO_MATIN_APRESMIDI' => $_GET['plo_matin_apresmidi']]);
+
+        if (is_null($plongee))
+            header('location: plongee');
+
+        if ( isset($_POST['submit']) )
+            $this->verification($plongee);
+
+        (new View('plongee/plongee_show/plongee_show_index'))->generate([
+            'plongee' => $plongee,
+        ]);
+    }
+
+    private function add()
+    {
+        if ( isset($_POST['submit']) ) {
+            $plongeur = new Plongeur($_POST);
+            $this->verification($plongeur);
+
+        }
+    }
+
+    private function verification($plongeur)
+    {
+        // Fonction de vérification d'une plongée, utile ?
+    }
 }
--- controller/PlongeurController.php
+++ controller/PlongeurController.php
@@ -42,13 +42,13 @@ class PlongeurController extends _ControllerClass
     public function edit()
     {
         if (!isset($_GET['per_num']))
-            header('location: '.URL.'/plongeur');
+            header('location: plongeur');
 
         $plongeur = $this->plongeurManager->getOne([
             'PER_NUM' => $_GET['per_num']]);
 
         if (is_null($plongeur))
-            header('location: '.URL.'/plongeur');
+            header('location: plongeur');
 
         if ( isset($_POST['submit']) )
             $this->verification($plongeur);
@@ -93,7 +93,7 @@ class PlongeurController extends _ControllerClass
                 $plongeur[0]->getPersonne()[0]->setPerPrenom($prenom);
                 $plongeur[0]->setAptCode($aptitude);
                 $this->plongeurManager->update($plongeur);
-                header("Location: /plongeur");
+                header("Location: plongeur");
             } else
                 echo 'Personne déjà enregistrée.';
         }
