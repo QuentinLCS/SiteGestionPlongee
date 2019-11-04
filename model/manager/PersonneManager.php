3fa1bd920f5e245cd843342bdd04be18875b6f9e
Darkmayke <moreaubaptiste14@gmail.com>
2019-11-04 13:24:10 +0100

    Ajout d'une personne comme active

--- model/manager/PersonneManager.php
+++ model/manager/PersonneManager.php
@@ -56,6 +56,6 @@ class PersonneManager extends _Model
         if ($add)
             DataBase::$db->majDonnees("INSERT INTO ".self::$table." (PER_NOM, PER_PRENOM, PER_DATE_CERTIF_MED) VALUES ('".$object[0]->getPerNom()."', '".$object[0]->getPerPrenom()."', '".$object[0]->getPerDateCertifMed()."')");
         else
-            DataBase::$db->majDonnees("UPDATE ".self::$table." SET PER_NOM = '".$object[0]->getPerNom()."', PER_PRENOM = '".$object[0]->getPerPrenom()."', PER_DATE_CERTIF_MED = '".$object[0]->getPerDateCertifMed()."' WHERE PER_NUM = '".$object[0]->getPerNum()."'");
+            DataBase::$db->majDonnees("UPDATE ".self::$table." SET PER_NOM = '".$object[0]->getPerNom()."', PER_PRENOM = '".$object[0]->getPerPrenom()."', PER_DATE_CERTIF_MED = '".$object[0]->getPerDateCertifMed()."', PER_ACTIVE = '".$object[0]->getPerActive()."' WHERE PER_NUM = '".$object[0]->getPerNum()."'");
     }
 }
